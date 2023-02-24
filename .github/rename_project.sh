20a3dc33066b17c85b3445a32d09de98496b0e17
robgarciab <robgarciab@users.noreply.github.com>
2023-02-24 21:26:08 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="robgarciab"
+original_name="flask_sample"
+original_urlname="flask-sample"
+original_description="Awesome flask_sample created by robgarciab"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv flask_sample $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- flask_sample/ext/commands.py
+++ flask_sample/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from flask_sample.ext.auth import create_user
+from flask_sample.ext.database import db
+from flask_sample.models import Product
 
 
 def create_db():
